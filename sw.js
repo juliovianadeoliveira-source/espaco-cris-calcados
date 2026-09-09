86578d0ed46111155ee169c9c69f44e265b1cfc8
juliovianadeoliveira-source <juliovianadeoliveira@gmail.com>
2026-09-09 18:41:19 -0300

    Corrigir memória e sequência da conversa da Cris

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='espaco-cris-app-v6';
+const CACHE='espaco-cris-app-v7';
 const CORE=['/app.html','/index.html','/catalogo/','/manifest-loja.webmanifest','/manifest-revendedoras.webmanifest','/icons/espaco-cris-192.png','/icons/espaco-cris-512.png','/icons/espaco-cris-apple-180.png','/pwa.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
